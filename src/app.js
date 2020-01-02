d6bcb5907932ae0021e358d75b387f7e6427fe21
Carlos <carlos@upoutsourcing.com>
2020-01-02 15:09:55 -0300

    Initial structure of the React

---
+++ src/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import React from 'react'
+
+import Title from './title'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Title name='Something 3' />
+      </div>
+    )
+  }
+})
+
+export default App
