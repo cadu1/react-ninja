d106a0ba8053ccfb88c7fc646d4f26eb96729374
Carlos <carlos@upoutsourcing.com>
2020-01-02 17:00:53 -0300

    Understanding 'key' prop

---
+++ src/Square.js
@@ -0,0 +1,18 @@
+'use stric'
+
+import React from 'react'
+
+const Square = ({ color }) => (
+  <div style={{
+    backgroundColor: color,
+    height: '100px',
+    width: '100px'
+  }}
+  />
+)
+
+Square.defaultProps = {
+  color: 'blue'
+}
+
+export default Square
--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@
 import React from 'react'
 
 import Title from './title'
+import Square from './Square'
 
 const App = React.createClass({
   render: function () {
     return (
       <div className='container'>
         <Title />
+        <Square />
+        {['gray', 'yellow', 'black'].map((color) => (
+          <Square key={color} color={color}/>
+        ))}
       </div>
     )
   }
