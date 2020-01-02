8a17d0a3334c0d341a8a929147e6c50cb03f44e8
Carlos <carlos@upoutsourcing.com>
2020-01-02 16:34:36 -0300

    Redering component with pure function

--- src/title.js
+++ src/title.js
@@ -2,17 +2,12 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  getDefaultProps: function() {
-    return {
-      name: 'Unknow'
-    }
-  },
-  render: function () {
-    return (
-      <h1>{this.props.name}</h1>
-    )
-  }
-})
+const Title = (props) => (
+  <h1>{props.name}!</h1>
+)
+
+Title.defaultProps = {
+  name: 'Unknow'
+}
 
 export default Title
