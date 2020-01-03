915aab729f235cc46077bead2d2d87e84599789e
Carlos <carlos@upoutsourcing.com>
2020-01-03 09:20:55 -0300

    Using 'children' property

--- src/app.js
+++ src/app.js
@@ -4,15 +4,17 @@ import React from 'react'
 
 import Title from './title'
 import Square from './Square'
+import Button from './Button'
 
 const App = React.createClass({
   render: function () {
     return (
       <div className='container'>
         <Title />
+        <Button>Some Text</Button>
         <Square />
         {['gray', 'yellow', 'gray'].map((color, index) => (
-          <Square key={index} color={color}/>
+          <Square key={index} color={color} />
         ))}
       </div>
     )
